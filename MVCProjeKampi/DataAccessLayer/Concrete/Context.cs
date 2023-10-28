ae62e14b2d764f1ee4f609cdaf710815d0fc2f30
ayhanakkaya <ayhanakkayameb@gmail.com>
2023-10-28 10:07:04 -0700

    mvc projesi

---
+++ MVCProjeKampi/DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,28 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    //DbContext aktif etmek için hatanın üzerine gelip
+    //using.System.Data.Entity seçilir
+    public class Context : DbContext
+
+    {
+        //Sınıfları dahil edebilmek için sağ tık 
+        //using EntityLayer.Concrete eklenir
+        //sql e yansıyacak tablolar
+        public DbSet<About> Abouts { get; set; }
+        //About sınıf ismi iken Abouts tablo ismi olacak
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Content> Contents { get; set; }
+        public DbSet<Heading> Headings { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+
+    }
+}
